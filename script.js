44339a71d337b280efc52d328b73e6d179887f83
mjwang <wang.michellej@gmail.com>
2025-06-04 11:59:39 -0700

    implement start study and start break

--- script.js
+++ script.js
@@ -6,14 +6,23 @@ function updateTimerDisplay(minutes, seconds) {
 
 }
 
+let countdown;
+function clearTimer() {
+    clearInterval(countdown);
+}
+
+/**
+ * 
+ * @param {*} duration in SECONDS
+ */
 function startTimer(duration) {
     let minutes = Math.floor(duration / 60);
     let seconds = duration % 60;
     //call update Timer display with minutes, seconds remaining
     updateTimerDisplay(minutes, seconds)
 
-    const countdown = setInterval(() => {
-        duration -= 1;
+    countdown = setInterval(() => {
+        duration --;
         minutes = Math.floor(duration / 60);
         seconds = duration % 60;
         updateTimerDisplay(minutes, seconds)
@@ -21,9 +30,29 @@ function startTimer(duration) {
 
     setTimeout(() => {
         console.log("CLEARING TIMER")
-        clearInterval(countdown);
+        clearTimer();
     }, duration * 1000);
 
 }
 
-startTimer(20)
+const startStudySession = () => {
+    clearTimer()
+    startTimer(25 * 60)
+
+    const studyCount = document.getElementById('study-count')
+    const initialCount = parseInt(studyCount.textContent)
+    studyCount.innerText = initialCount + 1
+}
+
+const startStudyButton = document.getElementById('study-btn')
+startStudyButton.addEventListener('click', startStudySession)
+
+const startBreakButton = document.getElementById('break-btn')
+startBreakButton.addEventListener('click', () => {
+    clearTimer()
+    startTimer(5 * 60)
+
+    const breakCount = document.getElementById('break-count')
+    const initialCount = parseInt(breakCount.textContent)
+    breakCount.innerText = initialCount + 1
+})
